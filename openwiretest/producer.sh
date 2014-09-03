e68325e20f27325012a6e8101edc68e4d39652e7
ugol <ugol@redhat.com>
2014-09-03 16:01:38 +0200

    Added #!/bin/bash

--- openwiretest/producer.sh
+++ openwiretest/producer.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 AMQ_HOME=/opt/jboss-a-mq-6.1.0.redhat-379/
 java -jar $AMQ_HOME/extras/mq-client.jar producer --destination queue://testQueue --count 5000 --brokerUrl tcp://localhost:61616 --user admin --password admin  
